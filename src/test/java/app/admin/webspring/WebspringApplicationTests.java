7aab078f7927e628e932f9c80e52e60ce3deb353
Trong Nguyen <nnbaotrong@gmail.com>
2024-12-27 12:13:17 +0700

    code base

---
+++ src/test/java/app/admin/webspring/WebspringApplicationTests.java
@@ -0,0 +1,13 @@
+package app.admin.webspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
